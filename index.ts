826ce9dd7e56b1f8bae85df28886b82453c6ec8a
miyajiro <ryosuke00719@gmail.com>
2023-03-16 18:01:04 +0900

    エラーハンドリングを修正

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const app = express();
 const port = 3000;
@@ -24,6 +24,13 @@ app.post('/users', async (req: Request, res: Response) => {
     });
     return res.json(user);
   } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this email'
+        );
+      }
+    }
     return res.status(400).json(e);
   }
 });
