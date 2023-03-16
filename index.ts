557b021df6d650af937d9fe267cccf1a128eef86
miyajiro <ryosuke00719@gmail.com>
2023-03-16 17:49:39 +0900

    POST /users を追加

--- index.ts
+++ index.ts
@@ -4,11 +4,24 @@ import { PrismaClient } from '@prisma/client';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get('/users', async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   return res.json(users);
+});
+
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  return res.json(user);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
