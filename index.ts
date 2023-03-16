d6bcd08b3ecf83e1ee09340444ce01b9a950c219
miyajiro <ryosuke00719@gmail.com>
2023-03-16 17:48:42 +0900

    ユーザ情報の取得

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+const prisma = new PrismaClient();
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
