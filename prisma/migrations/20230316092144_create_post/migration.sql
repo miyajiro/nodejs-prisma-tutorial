5ecd220a1a94e7acd66d8faa6bc2680fdeeb4841
miyajiro <ryosuke00719@gmail.com>
2023-03-16 18:21:53 +0900

    npx prisma migrate dev --name create_post

---
+++ prisma/migrations/20230316092144_create_post/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
